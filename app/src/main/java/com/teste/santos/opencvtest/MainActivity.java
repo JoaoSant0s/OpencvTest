1bb751dab677c5ce34daf4a0011a02b777b6b103
JoaoSant0s <ricardoelric@gmail.com>
2016-09-14 22:18:28 -0300

    Preparing initial capture of Frames. Transform Frame in Canny Image.

--- app/src/main/java/com/teste/santos/opencvtest/MainActivity.java
+++ app/src/main/java/com/teste/santos/opencvtest/MainActivity.java
@@ -3,23 +3,102 @@ package com.teste.santos.opencvtest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.SurfaceView;
 
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
+import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
 
     private static final String TAG = "MainActivity";
+    private JavaCameraView cameraView;
+    private Mat currentRgba;
+    private Mat imgGray;
+    private Mat imgCanny;
 
-    static {
-        if(OpenCVLoader.initDebug()){
-            Log.d(TAG, "Opencv Successly loaded");
-        }else {
-            Log.d(TAG, "Opencv Not loaded");
-        }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        currentRgba = new Mat(height, width, CvType.CV_8UC4);
+        imgGray = new Mat(height, width, CvType.CV_8UC1);
+        imgCanny = new Mat(height, width, CvType.CV_8UC1);
     }
+
+    @Override
+    public void onCameraViewStopped() {
+        currentRgba.release();
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        currentRgba = inputFrame.rgba();
+
+        Imgproc.cvtColor(currentRgba, imgGray, Imgproc.COLOR_RGB2GRAY);
+
+        Imgproc.Canny(imgGray, imgCanny, 50, 150);
+        return imgCanny;
+    }
+
+
+    private BaseLoaderCallback loaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch(status){
+                case BaseLoaderCallback.SUCCESS:{
+                    cameraView.enableView();
+                    break;
+                }
+                default:{
+                    super.onManagerConnected(status);
+                    break;
+                }
+            }
+            super.onManagerConnected(status);
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        cameraView = (JavaCameraView)findViewById(R.id.java_camera_view);
+
+        cameraView.setVisibility(SurfaceView.VISIBLE);
+        cameraView.setCvCameraViewListener(this);
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        if(cameraView != null){
+            cameraView.disableView();
+        }
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        if(cameraView != null){
+            cameraView.disableView();
+        }
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        if(OpenCVLoader.initDebug()){
+            Log.d(TAG, "Opencv Successly loaded");
+            loaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }else {
+            Log.d(TAG, "Opencv Not loaded");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_9, this, loaderCallback);
+        }
     }
 }
