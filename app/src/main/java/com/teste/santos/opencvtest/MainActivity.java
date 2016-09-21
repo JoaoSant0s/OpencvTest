18d89f2f57631f9a27c0e7ce06147dd0db647272
JoaoSant0s <ricardoelric@gmail.com>
2016-09-20 22:04:51 -0300

    Refactoring code, to maximaze modularization.

--- app/src/main/java/com/teste/santos/opencvtest/MainActivity.java
+++ app/src/main/java/com/teste/santos/opencvtest/MainActivity.java
@@ -1,8 +1,6 @@
 package com.teste.santos.opencvtest;
 
 import android.app.Activity;
-import android.os.Debug;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -11,20 +9,15 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
-import com.teste.santos.base.ColorBlobDetector;
-
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.JavaCameraView;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
-import org.opencv.core.Core;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfInt;
 import org.opencv.core.MatOfPoint;
-import org.opencv.core.Point;
-import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
@@ -34,17 +27,12 @@ import java.util.List;
 
 public class MainActivity extends Activity implements View.OnTouchListener, CameraBridgeViewBase.CvCameraViewListener2 {
 
-    private static final String  TAG              = "OCVSample::Activity";
+    private static final String  TAG = "OCVSample::Activity";
+    private Mat mRgba;
 
-    private boolean              mIsColorSelected = false;
-    private Mat                  mRgba;
-    private Scalar               mBlobColorRgba;
-    private Scalar               mBlobColorHsv;
-    private ColorBlobDetector mDetector;
-    private Mat                  mSpectrum;
-    private Size                 SPECTRUM_SIZE;
-    private Scalar               CONTOUR_COLOR_1;
-    private Scalar               CONTOUR_COLOR_2;
+    private Scalar CONTOUR_COLOR_RED;
+    private Scalar CONTOUR_COLOR_BLUE;
+    private Scalar CONTOUR_COLOR_GREEN;
 
     private Mat imgGray;
     private Mat imgBlur;
@@ -53,6 +41,9 @@ public class MainActivity extends Activity implements View.OnTouchListener, Came
     private Mat drawing;
     private MatOfInt currentHull;
     private List<MatOfPoint> contours;
+
+    private int saveHeight;
+    private int saveWidth;
 
     int THRESHOLD_VALUE = 70;//TODO: test with 0
     private static final int  MAX_BINARY_VALUE = 255;
@@ -121,21 +112,32 @@ public class MainActivity extends Activity implements View.OnTouchListener, Came
     }
     @Override
     public void onCameraViewStarted(int width, int height) {
-        mRgba = new Mat(height, width, CvType.CV_8UC4);
+        setDefaultValues(width, height);
+        resetValues();
+    }
 
-        imgGray = new Mat(height, width, CvType.CV_8UC1);
-        imgBlur = new Mat(height, width, CvType.CV_8UC1);
-        thresh1 = new Mat(height, width, CvType.CV_8UC1);
-        hierarchy = new Mat(height, width, CvType.CV_8UC1);
-        drawing = new Mat(height, width, CvType.CV_8UC4);
+    private void setDefaultValues(int width, int height){
+        mRgba = new Mat(height, width, CvType.CV_8UC4);
+        CONTOUR_COLOR_RED = new Scalar(255,0,0);
+        CONTOUR_COLOR_GREEN = new Scalar(0,255,0);
+        CONTOUR_COLOR_BLUE = new Scalar(0,0,255);
+
         contours = new ArrayList<>();
-        CONTOUR_COLOR_1 = new Scalar(0,255,0);
-        CONTOUR_COLOR_2 = new Scalar(0,0,255);
+        this.saveHeight = height;
+        this.saveWidth = width;
+    }
+
+    private void resetValues(){
+        imgGray = new Mat(this.saveHeight, this.saveWidth, CvType.CV_8UC1);
+        imgBlur = new Mat(this.saveHeight, this.saveWidth, CvType.CV_8UC1);
+        thresh1 = new Mat(this.saveHeight, this.saveWidth, CvType.CV_8UC1);
+        hierarchy = new Mat(this.saveHeight, this.saveWidth, CvType.CV_8UC1);
+        drawing = new Mat(this.saveHeight, this.saveWidth, CvType.CV_8UC4);
         currentHull = new MatOfInt();
+        contours.clear();
     }
     @Override
     public void onCameraViewStopped() {
-
         mRgba.release();
     }
 
@@ -143,7 +145,7 @@ public class MainActivity extends Activity implements View.OnTouchListener, Came
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
         mRgba = inputFrame.rgba();
 
-        contours.clear();
+        resetValues();
         //Take a Gray Frame
         Imgproc.cvtColor(mRgba, imgGray, Imgproc.COLOR_RGB2GRAY);
         //Take a Blur Frame
@@ -164,8 +166,6 @@ public class MainActivity extends Activity implements View.OnTouchListener, Came
                 currentPoint = i;
             }
         }
-        //TODO: MAKE OTHER ALTERNATIVE (USE PURE BLACK BACKGROUND)
-        drawing = new Mat(drawing.height(), drawing.width(), CvType.CV_8UC3);
 
         if(contours.isEmpty()) return drawing;
 
@@ -191,22 +191,13 @@ public class MainActivity extends Activity implements View.OnTouchListener, Came
         List<MatOfPoint> auxIntList =  new ArrayList<MatOfPoint>();
         auxIntList.add(mopOut);
 
-        Imgproc.drawContours(drawing, auxPointList, 0, CONTOUR_COLOR_1, 2);
-        Imgproc.drawContours(drawing, auxIntList, 0, CONTOUR_COLOR_2, 2);
+        Imgproc.drawContours(drawing, auxPointList, 0, CONTOUR_COLOR_RED, 2);
+        Imgproc.drawContours(drawing, auxIntList, 0, CONTOUR_COLOR_BLUE, 2);
 
         return drawing;
     }
 
     public boolean onTouch(View v, MotionEvent event) {
         return false; // don't need subsequent touch events
-    }
-
-
-    private Scalar converScalarHsv2Rgba(Scalar hsvColor) {
-        Mat pointMatRgba = new Mat();
-        Mat pointMatHsv = new Mat(1, 1, CvType.CV_8UC3, hsvColor);
-        Imgproc.cvtColor(pointMatHsv, pointMatRgba, Imgproc.COLOR_HSV2RGB_FULL, 4);
-
-        return new Scalar(pointMatRgba.get(0, 0));
     }
 }
